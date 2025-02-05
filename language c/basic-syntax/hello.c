8fd5c7b7b73f9692f13501b3f1343598682eb5c3
Luana Silva <luanasilva.developer@gmail.com>
2025-02-04 21:43:46 -0300

    feat: create a hello world program with c

---
+++ language c/basic-syntax/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // this is a header file library, header files add functionality to C programs.
+
+// this is a comment in C.
+
+int main() {
+  printf("Hello, World!");
+  return 0; // every statement ends with a semicolon
+}
